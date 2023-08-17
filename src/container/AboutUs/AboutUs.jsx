0b00466dacf3c066fafdc2cb7dbada6f23c70814
shreyaannnnn <shreyanbharadwaj@gmail.com>
2023-08-17 17:06:16 +0530

    new

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -8,22 +8,18 @@ const AboutUs = () => (
     className="app__aboutus app__bg flex__center section__padding"
     id="about"
   >
-    <div className="app__aboutus-overlay flex__center">
+    {/* <div className="app__aboutus-overlay flex__center">
       <img src={images.G} alt="G_overlay" />
-    </div>
+    </div> */}
 
     <div className="app__aboutus-content flex__center">
       <div className="app__aboutus-content_about">
         <h1 className="headtext__cormorant">About Us</h1>
         <img src={images.spoon} alt="about_spoon" className="spoon__img" />
         <p className="p__opensans">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra
-          adipiscing ultrices vulputate posuere tristique. In sed odio nec
-          aliquet eu proin mauris et.
+          bweehehheeheheheheh no
         </p>
-        <button type="button" className="custom__button">
-          Know More
-        </button>
+        
       </div>
 
       <div className="app__aboutus-content_knife flex__center">
@@ -34,13 +30,9 @@ const AboutUs = () => (
         <h1 className="headtext__cormorant">Our History</h1>
         <img src={images.spoon} alt="about_spoon" className="spoon__img" />
         <p className="p__opensans">
-          Adipiscing tempus ullamcorper lobortis odio tellus arcu volutpat.
-          Risus placerat morbi volutpat habitasse interdum mi aliquam In sed
-          odio nec aliquet.
+          ye bhi nai bataunga
         </p>
-        <button type="button" className="custom__button">
-          Know More
-        </button>
+        
       </div>
     </div>
   </div>
